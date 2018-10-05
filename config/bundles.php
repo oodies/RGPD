e139cf06706a586caab6fceeec6ff63123e8f2cb
schomy <sebastien.chomy@gmail.com>
2018-10-05 23:48:24 +0200

    build(composer): add the main dependencies of the project
    Add the profiler-pack symfony
    #composer require --dev symfony/profiler-pack
    
    Issue #1

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,14 @@
 <?php
-
+/**
+ * This file is part of RGPD project.
+ *
+ * (c) Sébastien CHOMY <sebastien.chomy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class     => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class               => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
