b2895ee341e36b87a7843a47b4ccda8458e1f466
schomy <sebastien.chomy@gmail.com>
2018-10-10 15:20:29 +0200

    feat(templates): legal notice generator
    - Add legal notice view
    
    issues #2

--- src/Controller/LegalNoticesGenerator.php
+++ src/Controller/LegalNoticesGenerator.php
@@ -55,6 +55,51 @@ class LegalNoticesGenerator
         $form = $this->formFactoryInterface->create('App\Form\LegalNoticesForm');
         $form->handleRequest($request);
 
+        if ($form->isSubmitted()) {
+            return new Response(
+                $this->twig->render(
+                    'App/legalNotice.html.twig',
+                    [
+                        // STUB Fake
+                        'domain'                     => 'oodigital.fr',
+                        'individual_owner_firstname' => 'Sébastien',
+                        'individual_owner_lastname'  => 'CHOMY',
+
+                        'society_owner_name'  => 'OODIGITAL',
+                        'society_entity_type' => 'SASU',
+
+                        'owner_streetAddress'   => '2b rue de la Houille Blanche',
+                        'owner_postalCode'      => '38190',
+                        'owner_addressLocality' => 'Villard Bonnot',
+                        'owner_addressCountry'  => 'France',
+                        'owner_email'           => 'sebastien.chomy@gmail.com',
+                        'owner_phone'           => '06 12 65 39 07',
+
+                        'capital_amount' => '1000 €',
+                        'duns_rcs'       => 'Grenoble 548 235 365',
+                        'duns_rm'        => '548 454 454',
+                        'vat_identifier' => 'FR 22 424 761 419',
+
+                        'licensed_profession_title'    => 'Agent immobilier',
+                        'licensed_profession_organism' => 'CCI de Grenoble',
+                        'licensed_profession_ref'      => 'Loi X',
+                        'licensed_profession_eu_state' => 'France',
+
+                        'hosting_name'            => 'OVH',
+                        'hosting_entity_type'     => 'SAS Société par action simplifié',
+                        'hosting_streetAddress'   => '2 rue Kellermann',
+                        'hosting_postalCode'      => '59100',
+                        'hosting_addressLocality' => 'Roubaix',
+                        'hosting_addressCountry'  => 'France',
+                        'hosting_capital_amount'  => '10 069 020 €',
+                        'hosting_duns_rcs'        => 'Grenoble 382 198 794',
+                        'hosting_phone'           => '1007',
+                        'hosting_email'           => 'support@ovh.com',
+                    ]
+                )
+            );
+        }
+
         return new Response(
             $this->twig->render(
                 'App/legalNoticeGenerator.html.twig',
